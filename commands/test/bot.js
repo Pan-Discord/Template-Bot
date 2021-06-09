4ad4edcd0603963042260ba90dbdedba93a58577
Pan-Discord <85648192+Pan-Discord@users.noreply.github.com>
2021-06-09 15:50:04 -0500

    Add files via upload

---
+++ commands/test/bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "comando",//aca el nombre del comando
+    description: "descri",//la descripción del comando
+
+    async run (client, message, args) {
+//code qe se ejecutara al poner el comando con su prefix
+    }
+}
